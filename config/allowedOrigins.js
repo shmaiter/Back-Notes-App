aa17a41cdcadb6d901aafd05a2e16a488682f320
Shmaither <shmaither@gmail.com>
2022-12-10 18:43:16 -0600

    bug: add cors options and allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["https://pinit-notes.onrender.com"];
+const allowedOrigins = ["https://pinit-notes.onrender.com", "https://pinit-notes-api.onrender.com"];
 //"http://localhost:3000",
 module.exports = allowedOrigins;
